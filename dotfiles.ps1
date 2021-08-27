e61f0b779b6e355a3a65b70f92df546834d93233
Laurent <gauthier.lrnt@gmail.com>
2021-08-27 21:46:03 +0200

    fix: Clean dotfile

--- dotfiles.ps1
+++
@@ -1,4 +0,0 @@
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y -r cmder
-choco install -y -r nodejs.install
